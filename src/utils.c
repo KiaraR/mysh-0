dc835d6b2c33cf0f0b1dcd77dadcfcd6618d8742
KiaraR <hcho2256@naver.com>
2017-09-30 12:37:10 +0900

    tried to edit utils.c but it doesn't go well. still don't know why:( The problem is const char*. here has strdup();

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,18 @@
 #include "utils.h"
+#include <string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+  //char *str = strdup(command);
+  char *p = strtok(command, " \n\t");
+  
+  while(p != NULL) {
+    argv[*argc++] = p;
+    p = strtok(NULL," ");
+  }
+  
+  for(int i=0;i<*argc;i++)
+    fprintf(stdin,"%s",argv);
 }
