fbccd61d2601b72cee97fcf2becf8ca4c80b5d72
KiaraR <hcho2256@naver.com>
2017-10-01 21:26:14 +0900

    I finally fixed const char*. The key was parsig const char* to (char*) before strtok func.:D Now i am HAPPPPPPPY!

--- src/utils.c
+++ src/utils.c
@@ -1,19 +1,15 @@
 #include "utils.h"
 #include <string.h>
 
-void mysh_parse_command(char* command,
+void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
-  char *str;
-  strcpy(command,str);
+  char *str = (char*)command;
   char *p = strtok(str, " \n\t");
   
   while(p != NULL) {
     *argv[*argc++] = p;
     p = strtok(NULL," ");
   }
-  
-  for(int i=0;i<*argc;i++)
-    printf("%s", *argv[i]);
 }
