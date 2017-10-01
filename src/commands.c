d95d5834112c098ca0fd63c59f3c330334990617
KiaraR <hcho2256@naver.com>
2017-10-01 23:21:08 +0900

    Editing vaild_pwd() func. I think i done my job for this func:) everything goes well until now

--- src/commands.c
+++ src/commands.c
@@ -9,12 +9,12 @@ char* dir;
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-  // TODO: Fill it!
-  if(strcmp(argv[1],"..")==0) {
+
+  if(strcmp(argv[1],"..")==0&&argc==1) {
     chdir("..");
-  } else if(strcmp(argv[1], ".")==0) {
+  } else if(strcmp(argv[1], ".")==0&&argc==1) {
     
-  } else {
+  } else if(argc==1){
     chdir(argv[1]);
   }  
   return 0;
@@ -35,6 +35,6 @@ int validate_cd_argv(int argc, char** argv) {
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(argc>1) return 0;
+  else return 1;
 }
