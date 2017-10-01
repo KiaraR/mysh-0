143a8bec4b81fc06846f08a72dc867ee6e541b43
KiaraR <hcho2256@naver.com>
2017-10-01 22:42:38 +0900

    Editing do_cd() but no exception handling

--- src/commands.c
+++ src/commands.c
@@ -10,7 +10,13 @@ int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
   // TODO: Fill it!
-  
+  if(strcmp(argv[1],"..")==0) {
+    chdir("..");
+  } else if(strcmp(argv[1], ".")==0) {
+    
+  } else {
+    chdir(argv[1]);
+  }  
   return 0;
 }
 
@@ -20,8 +26,6 @@ int do_pwd(int argc, char** argv) {
 
   dir = getcwd(NULL, BUFSIZ);
   printf("%s\n", dir);
-  // TODO: Fill it!
-
   return 0;
 }
 
